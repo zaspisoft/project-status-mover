c5350eacd15bc89cefdacfd228baafb18d09fca5
akazyti <akazyti@gmail.com>
2023-08-02 19:34:20 +0530

    fix: all items project

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 const core = require("@actions/core");
 const { graphql } = require("@octokit/graphql");
 
+
+const projectItemsQuery = (project_id, after) =>  `
+query{
+    node(id: "${project_id}") {
+  ... on ProjectV2 {
+    
+    items(first: 100,orderBy: {
+      field: POSITION,
+      direction:DESC
+    }${ after ? `, after: "${after}"`: ""}) {
+      totalCount
+      pageInfo {
+        hasNextPage,
+        endCursor
+      }
+      nodes{
+        id
+        content{ 
+          ...on Issue {
+            title
+            id
+            databaseId
+            number
+          }
+        }
+      }
+    }
+  }
+}
+}
+`
+
+
+const getAllProjectItems = async (project_id, graphqlWithAuth) => {
+	let payloads = []
+	let nextCursor = null;
+	while(true) {
+
+		const projectQueryItems = await graphqlWithAuth(projectItemsQuery(project_id, nextCursor))
+		console.log(projectQueryItems.node.items.pageInfo.hasNextPage)
+		payloads = [...payloads, ...projectQueryItems.node.items.nodes]
+		nextCursor = projectQueryItems.node.items.pageInfo.endCursor
+		if (!projectQueryItems.node.items.pageInfo.hasNextPage) {
+			break
+		}
+
+	}
+
+	return payloads
+}
+
+
 try {
   /**
    * Getting the variables
@@ -79,30 +131,7 @@ try {
 	const option_id = fieldNode.options.filter(each => each.name == status_value)[0].id
 
 
-	const allProjectItems = await graphqlWithAuth(`
-	query{ node(id: "${project_id}") {
-  ... on ProjectV2 { 
-    items(first: 100, orderBy: {
-      field: POSITION,
-      direction:DESC
-    })) {
-      nodes{
-        id 
-        
-        content{ 
-          ...on Issue {
-            title
-            id
-            databaseId
-            number
-          }
-        }
-      }
-    }
-  }
-}
-}
-	`)
+	const allProjectItems = await getAllProjectItems(project_id, graphqlWithAuth)
 
 	const itemId = allProjectItems.node.items.nodes.filter(each => each.content.databaseId == issue_id)[0].id
 	
